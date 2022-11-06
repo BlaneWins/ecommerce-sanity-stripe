20cbbd0b70061a3ee1c2432feb9224430c525f21
BlaneWinstead <blane.wins@gmail.com>
2022-11-06 15:54:04 -0600

    first commit

--- context/StateContext.js
+++ context/StateContext.js
@@ -91,6 +91,7 @@ export const StateContext = ({ children }) => {
             incQty,
             decQty,
             onAdd,
+            toggleCartItemQuantity,
             onRemove,
             setCartItems,
             setTotalPrice,
--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,5 +1,4 @@
 const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
-console.log(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
